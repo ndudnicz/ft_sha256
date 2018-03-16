17861901523a76c4a85757bbb465519caa5f0087
ndudnicz <ndudnicz@student.42.fr>
2018-03-16 17:01:39 +0100

    check S_ISREG(st_mode) before trying to map input file, no more segfault

--- src/map_file.c
+++ src/map_file.c
@@ -16,7 +16,7 @@ map_file(t_opt *options) {
 		return ((void*)((long int)ft_error("Error", "map_file()", OPEN_FAILED, 0)));
 	} else {
 		fstat(fd, &s);
-		if (S_ISREG(s.st_size)) {
+		if (S_ISREG(s.st_mode)) {
 			options->size = s.st_size;
 			ret = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 			return (ret ? ret : (void*)((long int)ft_error("Error", "map_file()", MMAP_FAILED, 0)));
